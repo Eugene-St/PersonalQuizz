d861447c44b9401b68376702b0614ff73c7627c8
Eugene St <Eugene.stepchenko@gmail.com>
2020-03-04 19:50:26 +0200

    Initial Commit

---
+++ PersonalQuizz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalQuizz
+//
+//  Created by Eugene St on 04.03.2020.
+//  Copyright © 2020 Eugene St. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
